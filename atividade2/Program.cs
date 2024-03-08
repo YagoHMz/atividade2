e2df6729d32911b343dfc75d2290c68a25b3038d
mahserrato <mahserrato14@gmail.com>
2024-03-07 23:54:10 -0300

    Add project files.

---
+++ atividade2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Double[] salario = new Double[5];
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Digite o salário de n° " + (i + 1));
+            Double.TryParse(Console.ReadLine(), out salario[i]);
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (salario[i] < 1000)
+            {
+                salario[i] = salario[i] + (salario[i] * 0.1);
+            }
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Salarios atualizadoss:");
+            Console.WriteLine("Salario " + (i + 1) + ": " + salario[i]);
+        }
+
+    }
+}
